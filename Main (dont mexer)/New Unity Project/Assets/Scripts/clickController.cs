ae3a9b84cea1b5681bb2eb8f9ab871f4b16a3d4c
Retchut <retch@users.noreply.github.com>
2020-01-31 19:24:10 +0000

    lots of shit

---
+++ Main (dont mexer)/New Unity Project/Assets/Scripts/clickController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clickController : MonoBehaviour
+{
+
+    public Button itemToFix;
+
+    [HideInInspector] public int itemIndex;
+
+    public Sprite[] itemSpriteArray;
+    [HideInInspector] public Sprite currentSprite;
+
+    public int[] toolRepairAmount;
+    [HideInInspector] public int currentRepairAmount;
+
+    public Slider durabilitySlider;
+    [HideInInspector] public int maxDurability;
+    [HideInInspector] public int currentDurability;
+
+    public int[] maxDurabilityArray;
+
+    public Text moneyText;
+    [HideInInspector] public int currentMoney;
+
+    void Start()
+    {
+        currentMoney = 0;
+        moneyText.text = "Money : " + currentMoney;
+        SelectNewItem();
+    }
+
+    public void SelectNewItem()
+    {
+        itemIndex = Random.Range(0, itemSpriteArray.Length);
+
+        currentSprite = itemSpriteArray[itemIndex];
+        maxDurability = maxDurabilityArray[itemIndex];
+
+        currentRepairAmount = 1;
+
+
+        itemToFix.image.sprite = currentSprite;
+        durabilitySlider.maxValue = maxDurability;
+        durabilitySlider.value = durabilitySlider.maxValue;
+        currentDurability = maxDurability;
+    }
+
+
+
+    public void WasClicked()
+    {
+        currentDurability = currentDurability - currentRepairAmount;
+        durabilitySlider.value = currentDurability;
+    }
+    
+}
