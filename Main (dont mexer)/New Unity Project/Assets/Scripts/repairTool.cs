9f4caca6eed414b55cbf42db1fd2825f9774991f
Retchut <retch@users.noreply.github.com>
2020-02-01 14:09:03 +0000

    repair

---
+++ Main (dont mexer)/New Unity Project/Assets/Scripts/repairTool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class repairTool : MonoBehaviour
+{
+
+    public int myCost;
+    public int myDurabilityNow;
+    public Button[] toolsInUse;
+    public int toolnumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClickToRepair()
+    {
+        if (FindObjectOfType<clickController>().currentMoney >= myCost)
+        {
+            FindObjectOfType<clickController>().currentMoney -= myCost;
+            toolsInUse[toolnumber].GetComponent<toolButton>().maxToolDurability = myDurabilityNow;
+            toolsInUse[toolnumber].GetComponent<toolButton>().Start2();
+        }
+        else
+        {
+            Debug.Log("Can't buy!");
+            /*
+            FindObjectOfType<clickController>().currentMoney -= myCost;
+            toolInUse.GetComponent<toolButton>().maxToolDurability = myDurabilityNow;
+            toolInUse.GetComponent<toolButton>().repairAmount = myRepairAmount;
+            toolInUse.GetComponent<toolButton>().Start2();
+            */
+        }
+    }
+}
--- Main (dont mexer)/New Unity Project/Assets/Scripts/upgraderScript.cs
+++ Main (dont mexer)/New Unity Project/Assets/Scripts/upgraderScript.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class upgraderScript : MonoBehaviour
 {
-    public int myDurabilityNow;
     public int myCost;
     public int myRepairAmount;
     public Button[] toolsInUse;
@@ -17,9 +16,7 @@ public class upgraderScript : MonoBehaviour
         if (FindObjectOfType<clickController>().currentMoney >= myCost)
         {
             FindObjectOfType<clickController>().currentMoney -= myCost;
-            toolsInUse[toolnumber].GetComponent<toolButton>().maxToolDurability = myDurabilityNow;
             toolsInUse[toolnumber].GetComponent<toolButton>().repairAmount = myRepairAmount;
-            toolsInUse[toolnumber].GetComponent<toolButton>().Start2();
         }
         else
         {
