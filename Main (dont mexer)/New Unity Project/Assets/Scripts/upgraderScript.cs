492e6c33ef5e8b935a062bce280d1a23ee2821e4
Joel <up201904977@fe.up.pt>
2020-02-02 05:55:12 +0000

    ijin

--- Main (dont mexer)/New Unity Project/Assets/Scripts/upgraderScript.cs
+++ Main (dont mexer)/New Unity Project/Assets/Scripts/upgraderScript.cs
@@ -11,18 +11,57 @@ public class upgraderScript : MonoBehaviour
     public Button[] toolsInUse;
     public int toolnumber;
     public Button mainButton;
+    [HideInInspector] public int level = 1;
+    public Text textLv;
+    public string str;
+    public Button me;
+    public int defaultRepairAmount;
+    public int defaultCost;
+    public string defaultText;
+    public Text text;
 
+    public Sprite lv10;
+    public Sprite lv20;
+    private void Update()
+    {
+
+        text.text = defaultText + myCost + "$";
+
+        if (level == 20)
+        {
+            toolsInUse[toolnumber].image.sprite = lv20;
+            me.interactable = false;
+        }
+        if (level >= 10 && level < 20)
+        {
+            toolsInUse[toolnumber].image.sprite = lv10;
+        }
+        if (toolsInUse[toolnumber].GetComponent<toolButton>().sliderOfDurability.value == 0)
+        {
+            level = 1;
+            textLv.text = str + " " + level.ToString();
+            toolsInUse[toolnumber].GetComponent<toolButton>().repairAmount = defaultRepairAmount;
+            mainButton.GetComponent<clickController>().currentRepairAmount = defaultRepairAmount;
+            myCost = defaultCost;
+        }
+    }
     public void IGotClicked()
     {
-        if (FindObjectOfType<clickController>().currentMoney >= myCost)
+        if (level < 20)
         {
-            FindObjectOfType<clickController>().currentMoney -= myCost;
-            toolsInUse[toolnumber].GetComponent<toolButton>().repairAmount += myRepairAmountIncrement;
-            mainButton.GetComponent<clickController>().currentRepairAmount += myRepairAmountIncrement;
-        }
-        else
-        {
-            Debug.Log("Can't buy!");
+            if (FindObjectOfType<clickController>().currentMoney >= myCost)
+            {
+                FindObjectOfType<clickController>().currentMoney -= myCost;
+                toolsInUse[toolnumber].GetComponent<toolButton>().repairAmount += myRepairAmountIncrement;
+                mainButton.GetComponent<clickController>().currentRepairAmount += myRepairAmountIncrement;
+                level += 1;
+                myCost = myCost + (myCost * 2) / level;
+                textLv.text = str + " " + level.ToString();
+            }
+            else
+            {
+                Debug.Log("Can't buy!");
+            }
         }
     }
 }
